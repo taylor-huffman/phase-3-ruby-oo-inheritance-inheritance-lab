20d4a74896b9b723879feb011073993134768904
Taylor Huffman <taylor@crucialartists.com>
2022-05-13 15:45:41 -0500

    commit tree 95ce23db450aa690e2cffee4f6f8ff2ee0cb4291

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(knowledge)
+        @knowledge.push(knowledge)
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
